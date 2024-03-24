1cd7275245ff86f602f2867345e6b2f266945e20
arunprasaad2711 <arunprasaad2711@gmail.com>
2024-03-25 00:43:46 +0530

    Templatized the index page using Jinja, updated some jinga templates. Added footer for the website.

---
+++ MakeWebsite.py
@@ -0,0 +1,34 @@
+from jinja2 import Environment, FileSystemLoader
+from os import system as sys
+from pathlib import Path
+from WebsiteData import *
+
+# Specify the template directory and environment 
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+i = 0
+for page in main_pages:
+	
+	data = {
+		'title' : titles[i],
+		'BannerImage01' : BannerImages[i][0],
+		'BannerImage02' : BannerImages[i][1],
+		'BannerImage03' : BannerImages[i][2]
+	}
+
+	# Templating Page
+	template = env.get_template('main_pages/'+page)
+	output = template.render(post=data)
+	
+	# Create Page
+	with open(page, 'w') as f:
+		print(output, file=f)
+	
+	print(f"Created {page} page!")
+	i += 1
+
+try:
+    sys("rm ./temp.html")
+except:
+    sys("del ./temp.html")
